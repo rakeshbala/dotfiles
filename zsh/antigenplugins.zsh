dee5ad79203e37d505c0f663069693e9d34ccfc5
Rakesh Balasubramanian <rakesh.balasubramanian@factset.com>
2016-04-18 22:29:11 -0400

    fix(startup): clean some bundles, add profiling setup

--- zsh/antigenplugins.zsh
+++ zsh/antigenplugins.zsh
@@ -3,9 +3,6 @@ source $DOTFILEDIR/zsh/antigen/antigen.zsh
 # Load the oh-my-zsh's library.
 antigen use oh-my-zsh
 
-# Bundles from the default repo (robbyrussell's oh-my-zsh).
-antigen bundle command-not-found
-
 # Syntax highlighting bundle.
 antigen bundle zsh-users/zsh-syntax-highlighting
 
@@ -16,8 +13,6 @@ source $DOTFILEDIR/zsh/$(platform).theme
 antigen bundle colored-man-pages
 antigen bundle djui/alias-tips
 antigen bundle colorize
-antigen bundle autojump
-antigen bundle supercrabtree/k
 antigen bundle arzzen/calc.plugin.zsh
 antigen bundle fasd
 antigen bundle vi-mode
