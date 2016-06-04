540971f3c65db1d0a04239b1543ea8cec6c91d76
Rakesh Balasubramanian <rakesh.balasubramanian@gmail.com>
2016-06-04 10:55:23 -0400

    add zplug

---
+++ zsh/zplugs.zsh
@@ -0,0 +1,27 @@
+export ZPLUG_HOME=/usr/local/opt/zplug
+source $ZPLUG_HOME/init.zsh
+
+# Syntax highlighting bundle.
+zplug "zsh-users/zsh-syntax-highlighting"
+zplug "zsh-users/zsh-history-substring-search"
+
+# Load the theme.
+zplug "themes/bureau", from:oh-my-zsh
+
+#my choices
+zplug "plugins/colored-man-pages", from:oh-my-zsh
+zplug "plugins/colorize", from:oh-my-zsh
+zplug "plugins/fasd", from:oh-my-zsh
+zplug "plugins/vi-mode", from:oh-my-zsh
+
+zplug "arzzen/calc.plugin.zsh"
+zplug "djui/alias-tips"
+zplug "zplug/zplug"
+
+# Tell zplug that you're done.
+zplug load
+#
+if ! zplug check; then
+    zplug install
+fi
+
